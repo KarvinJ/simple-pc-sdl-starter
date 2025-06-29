d49aed97afe803ea1b69d7d642711eca90e5aadd
knight <karvin_jimenez@hotmail.com>
2025-06-29 13:31:20 -0400

    updating library.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "inc/SDL.h"
 
-SDL_Window* window = nullptr;
-SDL_Renderer* renderer = nullptr;
+SDL_Window *window = nullptr;
+SDL_Renderer *renderer = nullptr;
 SDL_Rect rectangle;
 
 const int SPEED = 600;
@@ -10,53 +10,51 @@ const int SCREEN_WIDTH = 960;
 const int SCREEN_HEIGHT = 544;
 const int FRAME_RATE = 60; // Desired frame rate (frames per second)
 
-//Exit the game and clean up
-void quitGame() {
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-}
+bool isGameRunning = true;
 
 // Function to handle events
-void handleEvents() {
-
+void handleEvents()
+{
     SDL_Event event;
 
-    while (SDL_PollEvent(&event)) {
-
-        if (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE) {
-            
-            quitGame();
-            exit(0);
+    while (SDL_PollEvent(&event))
+    {
+        if (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE)
+        {
+            isGameRunning = false;
         }
     }
 }
 
 // Function to update rectangle movement.
-void update(float deltaTime) {
+void update(float deltaTime)
+{
+    const Uint8 *currentKeyStates = SDL_GetKeyboardState(NULL);
 
-    const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
-
-    if (currentKeyStates[SDL_SCANCODE_W] && rectangle.y > 0) {
+    if (currentKeyStates[SDL_SCANCODE_W] && rectangle.y > 0)
+    {
         rectangle.y -= SPEED * deltaTime;
     }
 
-    else if (currentKeyStates[SDL_SCANCODE_S] && rectangle.y < SCREEN_HEIGHT - rectangle.h) {
+    else if (currentKeyStates[SDL_SCANCODE_S] && rectangle.y < SCREEN_HEIGHT - rectangle.h)
+    {
         rectangle.y += SPEED * deltaTime;
     }
 
-    else if (currentKeyStates[SDL_SCANCODE_A] && rectangle.x > 0) {
+    else if (currentKeyStates[SDL_SCANCODE_A] && rectangle.x > 0)
+    {
         rectangle.x -= SPEED * deltaTime;
     }
 
-    else if (currentKeyStates[SDL_SCANCODE_D] && rectangle.x < SCREEN_WIDTH - rectangle.w) {
+    else if (currentKeyStates[SDL_SCANCODE_D] && rectangle.x < SCREEN_WIDTH - rectangle.w)
+    {
         rectangle.x += SPEED * deltaTime;
     }
 }
 
 // Function to render graphics
-void render() {
+void render()
+{
     // Clear the renderer
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
@@ -72,33 +70,37 @@ void render() {
 }
 
 // Function to cap frame rate
-void capFrameRate(Uint32 frameStartTime) {
-
+void capFrameRate(Uint32 frameStartTime)
+{
     Uint32 frameTime = SDL_GetTicks() - frameStartTime;
-    
-    if (frameTime < 1000 / FRAME_RATE) {
+
+    if (frameTime < 1000 / FRAME_RATE)
+    {
         SDL_Delay(1000 / FRAME_RATE - frameTime);
     }
 }
 
 #undef main
 
-int main() {
-
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+int main()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
         std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
         return 1;
     }
 
     window = SDL_CreateWindow("My Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (window == nullptr) {
+    if (window == nullptr)
+    {
         std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
     }
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (renderer == nullptr) {
+    if (renderer == nullptr)
+    {
         std::cerr << "Failed to create renderer: " << SDL_GetError() << std::endl;
         SDL_DestroyWindow(window);
         SDL_Quit();
@@ -112,14 +114,14 @@ int main() {
     float deltaTime = 0.0f;
 
     // Main loop
-    while (true) {
-        
+    while (isGameRunning)
+    {
         currentFrameTime = SDL_GetTicks();
 
-    // Calculate delta time in seconds
+        // Calculate delta time in seconds
         deltaTime = (currentFrameTime - previousFrameTime) / 1000.0f; // Convert to seconds
 
-    // Update the previous frame time for the next iteration
+        // Update the previous frame time for the next iteration
         previousFrameTime = currentFrameTime;
 
         handleEvents();
@@ -129,5 +131,7 @@ int main() {
         // capFrameRate(currentFrameTime);
     }
 
-    quitGame();
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
